1507e05a9940df1dd13bc6fb278aa5e99580c222
SomeMoosery <carterklein13@gmail.com>
2017-11-07 13:25:25 -0600

    Began working in db/seeds.rb to create main categories and subcategories -- NEED TO FLESH OUT

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#name based on community in schema.rb. where(...).first_or_create(...) means you only make this once even if run seed multiple times
+#use rake db:seed to create / run all this, rails c to open console
+community_category = Category.where(name: 'community').first_or_create(name: 'community')
+housing_category = Category.where(name: 'housing').first_or_create(name: 'housing')
+
+Subcategory.where(name: 'activities', category_id: community_category.id).first_or_create(name: 'activities', category_id: community_category.id)
+Subcategory.where(name: 'classes', category_id: community_category.id).first_or_create(name: 'classes', category_id: community_category.id)
+Subcategory.where(name: 'events', category_id: community_category.id).first_or_create(name: 'events', category_id: community_category.id)
