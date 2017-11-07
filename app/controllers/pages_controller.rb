e5055d238185b14627a3993af865f156efd29984
SomeMoosery <carterklein13@gmail.com>
2017-11-06 22:59:50 -0600

    Initial commit - basic functionality and temp static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def help
+  end
+
+  def scams
+  end
+
+  def terms
+  end
+
+  def safety
+  end
+
+  def privacy
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
