e2d4d8da3a36cb704ed65788f278426d126d5747
SomeMoosery <carterklein13@gmail.com>
2017-11-07 21:15:53 -0600

    Added ability to create listing and dynamically display - still need to get there through categories/subcategories

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,22 @@
 class ListingsController < ApplicationController
 
   def new
+    @listing = Listing.new #calls on new method in listing model
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.save
+    redirect_to root_path
+  end
+
+  def show
+    @listing = Listing.find(params[:id]) #go into my listings model, and find the parameter that i'm passing into it!
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:title, :description, :city, :state, :zipcode)
   end
 
 end
